4044857486241370a183727e5877f93d3a9709bc
Amendo51 <78269161+Amendo51@users.noreply.github.com>
2021-07-21 15:59:00 -0500

    created forms to create new users

--- users/forms.py
+++ users/forms.py
@@ -3,12 +3,12 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import CustomUser
 
-class CustomeUserCreationForm(UserCreationForm):
-    class meta(UserCreationForm.meta):
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
         model = CustomUser
-        field = UserCreationForm.Meta.fields + ('age',)
+        fields = UserCreationForm.Meta.fields + ('age',)
 class CustomUserChangeForm(UserChangeForm):
 
     class Meta:
             model = CustomUser
-            fields = UserChangeForm.fields
+            fields = UserChangeForm.Meta.fields
